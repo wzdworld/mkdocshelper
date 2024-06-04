406db2efb491954cdf57ca387aee9257bfcb9abc
wzdworld <171532452+wzdworld@users.noreply.github.com>
2024-06-04 15:29:48 +0800

    commit

--- .helper/deploy.bat
+++ .helper/deploy.bat
@@ -5,6 +5,7 @@ git config user.email 171532452+wzdworld@users.noreply.github.com
 git add *
 git commit -m "commit"
 git remote add origin git@github-wzdworld:wzdworld/mkdocshelper.git
+git remote set-url origin git@github-wzdworld:wzdworld/mkdocshelper.git
 git push -u origin main
 mkdocs gh-deploy
 exit
